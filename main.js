e5931ea6b79549565ca0a1e8c58406a9e6267478
Nathan <urwin.nathan@gmail.com>
2024-07-11 08:44:49 +0100

    create basic func outlines

---
+++ main.js
@@ -0,0 +1,30 @@
+// use no built-in methods
+
+function createBinarySearchTree () {
+    // array to be made into BST
+    const array = [];
+    // clean array (remove duplicate values)
+    function cleanArray () {
+
+    }
+    // set middle element of array as root 
+    function setRoot () {
+
+    }
+    // sorts array
+    function sortArray () {
+
+    }
+    // takes sorted array and returns binary search tree
+    function arrayToBST() {
+        // recursion
+        // set base case: if reached end of input array
+        // keep moving index along subarrays attaching the middle elements
+        // get subarray left half (lower half) of sorted array
+        // get middle of left half
+        // attach to root element as left child
+        // get subarray right half (higher half) of sorted array
+        // get middle of right half
+        // attach to root element as right child
+    }
+}
