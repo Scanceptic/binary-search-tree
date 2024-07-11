2ff33888ccf09e56effd1f3c1b181052c5f8d9b2
Nathan <urwin.nathan@gmail.com>
2024-07-11 09:18:18 +0100

    create bst function

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-// use no built-in methods
-
 // Node factory function
 function Node(value = null, leftChild = null, rightChild = null) {
 	return {
 		value,
-		leftChild,
-		rightChild,
+		left,
+		right,
 	};
 }
 
@@ -16,21 +14,39 @@ function Tree(array) {
 
 // Build Tree func
 function buildTree(array) {
+	// sort array FIRST so duplicates can be removed
+	function sortArray(array) {
+		return array.sort((a, b) => a - b);
+	}
 	// remove duplicate values
-	function cleanArray() {}
-	// sort array
-	function sortArray() {}
-	function arrayToBST() {
+	function cleanArray(array) {
+		return [...new Set(array)];
+	}
+	// make array into BST
+	function arrayToBST(array, start = 0, end = array.length - 1) {
 		// recursion
 		// set base case: if reached end of input array
-		// keep moving index along subarrays attaching the middle elements
-		// get subarray left half (lower half) of sorted array
-		// get middle of left half
-		// attach to root element as left child
-		// get subarray right half (higher half) of sorted array
-		// get middle of right half
-		// attach to root element as right child
+		if (start > end) {
+			return null;
+		}
+		// get root element of input array
+		const root = parseInt((start + end) / 2);
+		const node = Node(array[root]);
+		// attach left node of root = root of the left subarray
+		node.left = arrayToBST(array, start, root - 1);
+		// attach right node of root = root of the right subarray
+		node.right = arrayToBST(array, root + 1, end);
+		// keep looping until every node in the array has been attached
+		return node;
 	}
+	// sort for cleaner to work
+	const sortedArray = sortArray(array);
+	// clean for builder to work
+	const cleanedArray = cleanArray(sortedArray);
+	// build
+	const builtBST = arrayToBST(cleanedArray);
+	// return root node
+	return builtBST;
 }
 
 // Provided BST print function
